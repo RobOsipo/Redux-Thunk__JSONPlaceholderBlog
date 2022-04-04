b2d5591184ee07754f5e404746f8213fd0e7af42
Robert <osipovitchr@gmail.com>
2022-04-04 10:37:27 -0400

    redux setup/dummy reducers + App/Post components

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import PostList from './PostList.js'
+
+
+const App = () => {
+    return (
+        <main className="ui container">
+            <PostList />
+        </main>
+    )
+}
+
+export default App;
---
+++ src/reducers/index.js
@@ -0,0 +1,5 @@
+import { combineReducers } from "redux";
+
+export default combineReducers({
+    dummyReducerReplaceMe: () => 'im a temp dummy reducer'
+})
